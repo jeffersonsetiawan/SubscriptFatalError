64dc80ba60460a1bde1e034299dab38bb718db5a
Jefferson Setiawan <jefferson.setiawan@icloud.com>
2020-06-03 14:52:52 +0700

    Initial Commit

---
+++ SubscriptFatalError/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SubscriptFatalError
+//
+//  Created by Jefferson Setiawan on 03/06/20.
+//  Copyright © 2020 Jefferson Setiawan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
